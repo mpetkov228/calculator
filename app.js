532ba7b6bde7a1ae5f9159da20646765c5155172
mpetkov228 <mpetkov228@gmail.com>
2023-06-05 21:39:03 +0300

    Add operate function

--- app.js
+++ app.js
@@ -13,3 +13,22 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(num1, num2, operator) {
+    let result;
+    switch (operator) {
+        case '+':
+            result = add(num1, num2);
+            break;
+        case '-':
+            result = subtract(num1, num2);
+            break;
+        case '*':
+            result = multiply(num1, num2);
+            break;
+        case '/':
+            result = divide(num1, num2);
+            break;
+    }
+    return result;
+}
